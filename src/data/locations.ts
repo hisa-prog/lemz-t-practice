b013973dd439ad5046bce5113410c70ad4c060cd
hisa <vladzavzatov@gmail.com>
2023-02-20 16:29:18 +0700

    fixed: locations

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -1,23 +1,7 @@
 export const locationsRepair : Array<{name: string, img: string}> = [
     {
-        name: 'Battery Pack Capasitor 1',
-        img: 'images/locations/capasitor1.jpg',
-    },
-    {
-        name: 'Battery Pack Capasitor 2',
-        img: 'images/locations/capasitor2.jpg',
-    },
-    {
-        name: 'Battery Pack Capasitor 3',
-        img: 'images/locations/capasitor3.jpg',
-    },
-    {
-        name: 'Battery Pack Capasitor 4',
-        img: 'images/locations/capasitor4.jpg',
-    },
-    {
-        name: 'Battery Pack Capasitor 5',
-        img: 'images/locations/capasitor5.jpg',
+        name: 'Battery Pack Capasitor',
+        img: 'images/locations/capasitor.jpg',
     },
     {
         name: 'Plasma Heater',
