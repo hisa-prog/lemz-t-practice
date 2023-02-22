ebf0bee48ccf425aa08a3a4cc28d9d9f69ce7555
hisa <vladzavzatov@gmail.com>
2023-02-22 14:42:50 +0700

    fixed: mergered conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import BatteryPackContextProvider from "./components/context/batteryPackContext";
+import PlasmaHeaterContextProvider from "./components/context/plasmaHeaterContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <App />
+    <BatteryPackContextProvider>
+      <PlasmaHeaterContextProvider>
+        <App />
+      </PlasmaHeaterContextProvider>
+    </BatteryPackContextProvider>
   </React.StrictMode>
 );
 
